939dec6cdd11413707794b380122127a517c06e7
Morgan Pritchard <morgan.pritchard@fhnw.edu>
2026-04-08 11:43:39 -0600

    Added Date to errorHandler

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -2,10 +2,16 @@ const { logMW, logEvents } = require("./log")
 
 function onError(err, req, res, next) {
     try {
+        let errorObj = {
+            date: new Date().toLocaleString(`en-US`),
+            error: err.message
+        }
+        console.log(err);
+        
         logMW(req, res, next)
         res.status(err.status || 500).json({ error: err.message })
-        logEvents(err.message)
-        console.error(err.message)
+        logEvents(JSON.stringify(errorObj))
+        console.error(errorObj)
     } catch (error) {
         console.error('Error in error handler:', error)
         console.error(error.message)
